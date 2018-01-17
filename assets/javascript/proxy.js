b5dc082f3fe2bc3496aa2bac9714879c1ca9f2f4
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-01-16 21:01:03 -0800

    Removed utility function from proxy Moved it to utility.js

--- assets/javascript/proxy.js
+++ assets/javascript/proxy.js
@@ -20,16 +20,3 @@ function queryProxy(requestURL, apiKey){
 
    return response;
 }
-
-
-//converts your miles to meters because yelp wants it this way lol
-function milesToMeters(miles){
-
-	const metersInAMile = 1610; //rounded to nearest meter
-	const meters = miles * metersInAMile;
-
-	return meters;
-
-}
-
-
