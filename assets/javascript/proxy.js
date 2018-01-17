6e2bcbaa3f349e32436bf7454d12f385c454ad80
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-01-16 20:56:26 -0800

    Added function to convert Miles to Meters

--- assets/javascript/proxy.js
+++ assets/javascript/proxy.js
@@ -20,3 +20,16 @@ function queryProxy(requestURL, apiKey){
 
    return response;
 }
+
+
+//converts your miles to meters because yelp wants it this way lol
+function milesToMeters(miles){
+
+	const metersInAMile = 1610; //rounded to nearest meter
+	const meters = miles * metersInAMile;
+
+	return meters;
+
+}
+
+
