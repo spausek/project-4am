14dbd5cdcd535f59bf591a9fc71c26d61293a9cc
phild6596 <phild6596@gmail.com>
2018-01-17 21:07:58 -0800

    <JS has onclick feature to search and print band info with tour dates>

--- assets/javascript/band.js
+++ assets/javascript/band.js
@@ -1,8 +1,10 @@
+$(document).ready(function () {
+    
 function queryProxy(requestUrl, apiKey) {
 
     const proxy = 'https://proxy.bhsplex.com/';
     let response = undefined;
-    var requestUrl = 'http://api.bandsintown.com/artists/Guns%20N%20Roses.json?api_version=2.0&app_id=project-4am';
+    var requestUrl = 'http://api.bandsintown.com/artists/' + artistSearchTerm + '.json?api_version=2.0&app_id=project-4am';
     $.ajax({
         method: 'GET',
         url: proxy + requestUrl,
@@ -10,7 +12,6 @@ function queryProxy(requestUrl, apiKey) {
             Authorization: 'Bearer ' + apiKey
         }
     }).done(function (data) {
-        console.log(data)
         response = data;
         for (var i = 0; i < 1; i++) {
             var artist = data.name;
@@ -24,21 +25,18 @@ function queryProxy(requestUrl, apiKey) {
             artistResult.append(artist);
             artistShows.append(artistTourDates);
             upcomingShows.append("Upcoming shows: " + upcomingShowsCount);
-
             $(".artist").append(artistThumbImg, artistResult, upcomingShows, artistShows);
-        } console.log(artist);
-        console.log(artistImage);
-        console.log(upcomingShowsCount);
+        }
     })
 
     return response;
 }
-queryProxy();
+
 
 function queryShows(showRequestUrl, apiKey) {
     const proxy = 'https://proxy.bhsplex.com/';
     let response = undefined;
-    var showRequestUrl = "http://api.bandsintown.com/artists/Guns%20N%20Roses/events.json?api_version=2.0&app_id=project-4am";
+    var showRequestUrl = "http://api.bandsintown.com/artists/" + artistSearchTerm + "/events.json?api_version=2.0&app_id=project-4am";
     $.ajax({
         method: "GET",
         url: proxy + showRequestUrl,
@@ -58,41 +56,27 @@ function queryShows(showRequestUrl, apiKey) {
             //a venue name, let me know and I'll fix it.
             var yelpLatitude = data[i].venue.latitude;
             var yelpLongitude = data[i].venue.longitude;
-
-            console.log(shows);
-            console.log(showLocation);
-            // Geo Coords are here
-            console.log(yelpLatitude);
-            console.log(yelpLongitude);
             //Back to the boring stuff
-            console.log(dateTime);
-            console.log(saleDateTime);
-            console.log(ticketStatus);
-            console.log(ticketUrl);
+            var artistShowHeadline = $("<div class='show-headline'>");
+            var artistShowLocation = $("<div class='show-location'>");
+            var showDateTime = $("<div class='show-date-time'>");
+            var artistTicketStatus = $("<div class='ticket-status'>");
+            artistShowHeadline.append(shows);
+            artistShowLocation.append(showLocation);
+            showDateTime.append(dateTime);
+            artistTicketStatus.append("Ticket availability: " + ticketStatus);
+            $(".artist").append(artistShowHeadline, artistShowLocation, showDateTime, artistTicketStatus);
         }
-        console.log(data);
     })
 }
-queryShows();
 
-//function parseArtistJSON(artistJSON) {
-
-
-//    var artist = artistJSON.name;
-//    var artistImage = artistJSON.thumb_url;
-//    var upcomingShowsCount = artistJSON.upcoming_event_count;
-//    var artistTourDates = artistJSON.facebook_tour_dates_url;
-//    var artistResult = $("<div class='artistRow'>");
-//    var artistThumbImg = "<img class='artistImg' src='" + artistImage + "'>";
-//    var artistShows = $("<div class='artist-shows' src='" + artistTourDates + "'>");
-//    var upcomingShows = $("<div class='showCount'" + upcomingShowsCount + "'>");
-//    artistResult.append(artist);
-//    artistShows.append(artistTourDates);
-//    upcomingShows.append("Upcoming shows: " + upcomingShowsCount);
-
-//    $(".artist").append(artistThumbImg, artistResult, upcomingShows, artistShows);
-//    console.log(artist);
-//   console.log(artistImage);
-//    console.log(upcomingShowsCount);
-
-//}
+$("#artistSearch").on("click", function () {
+    event.preventDefault();
+    var artistTerm = $("#userInput").val().trim();
+    artistSearchTerm = artistTerm.split(" ").join("%20");
+    $("#userInput").val("");
+    $(".artist").empty();
+    queryProxy(artistSearchTerm);
+    queryShows(artistSearchTerm);
+})
+})
