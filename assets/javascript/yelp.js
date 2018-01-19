b3486257847c3515cca02a551f9c8c271b7a8280
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-01-18 20:43:27 -0800

    Business Object completed and queryURL function replaced Proxy.js

--- assets/javascript/yelp.js
+++ assets/javascript/yelp.js
@@ -1,32 +1,80 @@
+function createBusiness(jsonBusiness){
+
+
+	const Business = {
+		name: jsonBusiness.name,
+		phone: jsonBusiness.phone,
+		price : jsonBusiness.price,
+		rating : jsonBusiness.rating,
+		yelpURL : jsonBusiness.url,
+		imageURL : jsonBusiness.image_url,
+		location : jsonBusiness.location,
+		distance : metersToMiles(jsonBusiness.distance),
+
+	}
+
+	return Business;
+}
+
+
 function newYelpQuery(){
 
 	const YelpQuery = {
 
 		endpoint : "https://api.yelp.com/v3/businesses/search?",
 		key : 'placeholder',
-		locations : [],
+		businesses : [],
+		currentOffset : 0,
+		totalResults: 0,
 		params : {
 			location : undefined,
 			term : "&categories=bars,restaurants",
 			radius : undefined,
+			offset: "&offset=0", //on forwardclick, offset += limit. On backclic, offset -= limit
+			limit: "&limit=25", //25 per page/search
 		},
 		 
-		
 		setLocation : function(location){
-
 			this.params.location = "&location=" + location;
 		},
 		setRadius : function(miles){
 			this.params.radius = "&radius=" + milesToMeters(miles);
 		},
 		parseLocationData : function(jsonData){
+			const YelpQuery = this;
+			this.totalResults = jsonData.total;
 
-			//work in progress
+			jsonData.businesses.map(function(business){
+				YelpQuery.businesses.push(createBusiness(business))
+			});
+
 		},
-		query : function(){
-			const queryURL = this.endpoint + this.params.location + this.params.term + this.params.radius;
-			const key = this.key;
-			queryResults = this.parseLocationData(queryProxy(queryURL, key));
+		createQueryURL : function(){
+			let queryURL = this.endpoint + this.params.location + this.params.term + this.params.radius;
+			queryURL += this.params.limit + this.params.offset;
+			
+			return queryURL;
+		},
+		queryBusinesses(YelpQuery){
+
+
+		    const proxy = 'https://proxy.bhsplex.com/';
+		    let response = undefined;
+
+		    $.ajax({
+		        method: 'GET',
+		        // we simply append the yelp url to the proxy url
+		        url: proxy + YelpQuery.createQueryURL(),
+		        // doesn't matter if there's no key for bandsintown, it will just be null
+		        headers: {
+		            Authorization: 'Bearer ' + YelpQuery.key
+		        }
+		    })
+		    .done(function(data) {
+
+		        YelpQuery.parseLocationData(data);
+		        console.log(YelpQuery.businesses);
+		    });
 
 		}
 	}
