8cc2b6acfa0eea08e4626e8b5589d48eda069383
spausek <31174081+spausek@users.noreply.github.com>
2018-01-21 20:27:09 -0800

    Refactored map.js
    Removed create map function since it was reduntant.

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,36 +1,16 @@
-//Global Variable
+//Global variables for map and marker generation
 
 var latitudeData = [];
 console.log(latitudeData);
 var longitudeData = [];
 console.log(latitudeData);
-var firstMap;
+var map;
 
-// Creaters the object for the google map API function to consume
+//Function to make map
+     
+function initMap(){
+ map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 5,
+                });
 
-function createMap (){
-        var firstMap = {lat:latitudeData[0], lng:longitudeData[0]};
-        console.log(firstMap);
-        initMap(firstMap);
-        }
-
-//Generates map
-        
-function initMap(firstMap){
-        var newMap = {
-            zoom: 6,
-            center:firstMap
-            };
-
-// still need to trouble shoot marker creation
-
-console.log(firstMap);
-        var marker = new google.maps.Marker({
-            position: firstMap,
-            newMap: newMap
-            });
-            
-        var map = new google.maps.Map(document.getElementById('map'), newMap);
-        }
-
-// function to make multiple markers for concert locations. 
+                }
